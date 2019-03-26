ef87d5b9c7c88d45bd38a32359d410232294f4f1
ant33 <platovmm@mail.ru>
2019-03-26 23:46:40 +0300

    Add files via upload

---
+++ chapter_2/ex_2_45.rkt
@@ -0,0 +1,26 @@
+#lang planet neil/sicp
+
+;Exercise 2.45: right-split and up-split can be expressed
+;as instances of a general spliing operation. Define a procedure
+;split with the property that evaluating
+;
+;(define right-split (split beside below))
+;(define up-split (split below beside))
+;
+;produces procedures right-split and up-split with the
+;same behaviors as the ones already defined.
+
+(define (split proc1 proc2)
+  (define (pos-split painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller (pos-split painter (- n 1))))
+          (proc1 painter (proc2 smaller smaller)))))
+  (lambda (pic n)
+    (pos-split pic n)))
+
+(define right-split (split beside below))
+(define up-split (split below beside))
+
+(paint (up-split einstein 4))
+
