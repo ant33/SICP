c6ccad76c8d17a4e189ca4e568bb1fde10e13328
ant33 <platovmm@mail.ru>
2019-04-02 20:05:20 +0300

    Update ex_2_61.rkt

--- chapter_2/ex_2_61.rkt
+++ chapter_2/ex_2_61.rkt
@@ -1,4 +1,10 @@
 #lang racket
+; Exercise 2.61: Give an implementation of adjoin-set using
+; the ordered representation. By analogy with elementof-
+; set? show how to take advantage of the ordering to
+; produce a procedure that requires on the average about half
+; as many steps as with the unordered representation.
+
 
 (define (element-of-set? x set)
   (cond ((null? set) false)
